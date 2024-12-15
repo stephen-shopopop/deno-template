e04df6d142d9ac6d192d8da6331454378653fd85
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2024-12-15 18:48:05 +0100

    test: add test for global-context libray

---
+++ library/global-context/mod.test.ts
@@ -0,0 +1,63 @@
+import { context } from './mod.ts'
+import { describe, test } from 'jsr:@std/testing/bdd'
+import { expect } from 'jsr:@std/expect'
+import { assertSpyCall, assertSpyCalls, spy } from 'jsr:@std/testing/mock'
+
+describe('global-context', () => {
+  test('When instantiating a new context with initial context, then should return the initial context', () => {
+    // Arrange
+    const getCurrentContext = spy(() => context.getStore())
+
+    const initContext = { requestId: 'a' }
+
+    // Act
+    context.run({ ...initContext }, () => getCurrentContext())
+
+    // Assert
+    assertSpyCalls(getCurrentContext, 1)
+    assertSpyCall(getCurrentContext, 0, {
+      args: [],
+      returned: {
+        requestId: 'a',
+      },
+    })
+  })
+
+  test('When instantiating a new context object and add properties, then return keys/values of store', () => {
+    // Arrange
+    const requestId = crypto.randomUUID()
+    const getCurrentContext = spy(() => context.getStore())
+
+    let message: unknown
+
+    // Act
+    context.run({}, () => {
+      context.set('requestId', requestId)
+
+      getCurrentContext()
+
+      message = context.get('requestId')
+    })
+
+    // Assert
+    expect(message).toBe(requestId)
+    assertSpyCalls(getCurrentContext, 1)
+    assertSpyCall(getCurrentContext, 0, {
+      args: [],
+      returned: {
+        requestId,
+      },
+    })
+  })
+
+  test('When no instantiating a new context, then return undefined', () => {
+    // Arrange
+    context.set('requestId', crypto.randomUUID())
+
+    // Act
+    const getStore = context.getStore()
+
+    // Assert
+    expect(getStore).toEqual(undefined)
+  })
+})
